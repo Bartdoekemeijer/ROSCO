aeb976f7ebd29952909c4940741c086a64da0c74
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-07-21 13:03:25 -0600

    Move turbine_measurements to inside UpdateZeroMQ function

--- ROSCO/src/ReadSetParameters.f90
+++ ROSCO/src/ReadSetParameters.f90
@@ -56,7 +56,6 @@ CONTAINS
         TYPE(LocalVariables), INTENT(INOUT) :: LocalVar
 
         character(256) :: zmq_address
-        real(C_DOUBLE), dimension(0:14) :: turbine_measurements
         real(C_DOUBLE), dimension(0:4) :: setpoints
 
         ! Load variables from calling program (See Appendix A of Bladed User's Guide):
@@ -91,27 +90,12 @@ CONTAINS
 
         PRINT *,' Calling UpdateZeroMQ...'
         ! Collect measurements to be sent to ZeroMQ server
-        turbine_measurements(0) = LocalVar%iStatus
-        turbine_measurements(1) = LocalVar%Time
-        turbine_measurements(2) = LocalVar%VS_MechGenPwr
-        turbine_measurements(3) = LocalVar%VS_GenPwr
-        turbine_measurements(4) = LocalVar%GenSpeed
-        turbine_measurements(5) = LocalVar%RotSpeed
-        turbine_measurements(6) = LocalVar%GenTqMeas
-        turbine_measurements(7) = LocalVar%Y_M
-        turbine_measurements(8) = LocalVar%HorWindV
-        turbine_measurements(9) = LocalVar%rootMOOP(1)
-        turbine_measurements(10) = LocalVar%rootMOOP(2)
-        turbine_measurements(11) = LocalVar%rootMOOP(3)
-        turbine_measurements(12) = LocalVar%FA_Acc
-        turbine_measurements(13) = LocalVar%NacIMU_FA_Acc
-        turbine_measurements(14) = LocalVar%Azimuth
 
         ! Define the ZeroMQ IP address and port
         zmq_address = C_CHAR_"tcp://localhost:5555"//C_NULL_CHAR
 
         ! Call ZeroMQ function and exchange information
-        CALL UpdateZeroMQ(turbine_measurements, zmq_address, setpoints)
+        CALL UpdateZeroMQ(LocalVar, zmq_address, setpoints)
         write (*,*) "ZeroMQInterface: torque setpoint from ssc: ", setpoints(0)
         write (*,*) "ZeroMQInterface: yaw setpoint from ssc: ", setpoints(1)
         write (*,*) "ZeroMQInterface: pitch 1 setpoint from ssc: ", setpoints(2)
--- ROSCO/src/ZeroMQInterface.f90
+++ ROSCO/src/ZeroMQInterface.f90
@@ -4,8 +4,10 @@ module ZeroMQInterface
     ! 
 
 CONTAINS
-    SUBROUTINE UpdateZeroMQ(turbine_measurements, zmq_address, setpoints)
+    SUBROUTINE UpdateZeroMQ(LocalVar, zmq_address, setpoints)
+        USE ROSCO_Types, ONLY : LocalVariables
         IMPLICIT NONE
+        TYPE(LocalVariables), INTENT(IN) :: LocalVar
         character(256) :: zmq_address
         real(C_DOUBLE), dimension(0:4) :: setpoints
         real(C_DOUBLE), dimension(0:14) :: turbine_measurements
@@ -19,6 +21,24 @@ CONTAINS
                 real(C_DOUBLE) :: measurements(3), setpoints(3)
             end subroutine zmq_client
         end interface
+
+        ! Collect measurements to be sent to ZeroMQ server
+        turbine_measurements(0) = LocalVar%iStatus
+        turbine_measurements(1) = LocalVar%Time
+        turbine_measurements(2) = LocalVar%VS_MechGenPwr
+        turbine_measurements(3) = LocalVar%VS_GenPwr
+        turbine_measurements(4) = LocalVar%GenSpeed
+        turbine_measurements(5) = LocalVar%RotSpeed
+        turbine_measurements(6) = LocalVar%GenTqMeas
+        turbine_measurements(7) = LocalVar%Y_M
+        turbine_measurements(8) = LocalVar%HorWindV
+        turbine_measurements(9) = LocalVar%rootMOOP(1)
+        turbine_measurements(10) = LocalVar%rootMOOP(2)
+        turbine_measurements(11) = LocalVar%rootMOOP(3)
+        turbine_measurements(12) = LocalVar%FA_Acc
+        turbine_measurements(13) = LocalVar%NacIMU_FA_Acc
+        turbine_measurements(14) = LocalVar%Azimuth
+        ! ... add nacelle position...?
 
         ! zmq_address = C_CHAR_"tcp://localhost:5555"//C_NULL_CHAR
         call zmq_client(zmq_address, turbine_measurements, setpoints)
